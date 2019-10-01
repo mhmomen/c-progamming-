8bfd2bc75200da89426eb99dd0557ee0275cc8b8
momen <hemelmomin520@gmail.com>
2019-10-02 02:04:53 +0600

    problem2

--- 02-10-19/problem2/main.c
+++ 02-10-19/problem2/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int arr[30],n,i,small;
+    printf("\nEnter the th number of arry: ");
+    scanf("%d",&n);
+    printf("\nEnter the element number: ");
+    for(i=0;i<n; i++)
+        scanf("%d",&arr[i]);
+    small= arr[0];
+    for(i=1;i<n;i++)
+    {
+        if(small>arr[i])
+            small= arr[i];
+    }
+    printf("the smallest number:%d ",small);
     return 0;
 }
