e95a3e7bf6ddef51bf1d1d3e9ef1e3302ebdfc7d
momen <hemelmomin520@gmail.com>
2019-10-02 00:56:52 +0600

    arr problem1

--- arry1/main.c
+++ arry1/main.c
@@ -3,6 +3,15 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int arr[12],n,i,sum=0;
+    printf("Enter the number of student: ");
+    scanf("%d",&n);
+    for (i=1; i<=n; i++)
+    {
+        printf("the mark of student %d :",i);
+        scanf("%d",&arr[i]);
+        sum+=arr[i];
+    }
+    printf("sum is %d",sum);
     return 0;
 }
