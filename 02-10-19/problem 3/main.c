199cce733e6f30ad97d44ef9523e151d1a2a79f4
momen <hemelmomin520@gmail.com>
2019-10-02 21:58:59 +0600

    problem3

--- 02-10-19/problem 3/main.c
+++ 02-10-19/problem 3/main.c
@@ -3,6 +3,26 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int n, i,j, a[20],t;
+    printf("Enter the number of input:");
+    scanf("%d",&n);
+    printf("\nthe input number");
+    for(i=1;i<=n;i++)
+        scanf("%d",&a[i]);
+    for(i=1;i<=n;i++)
+    {
+        for(j=i+1;j<=n;j++)
+        {
+            if(a[i]>a[j])
+            {
+               t=a[i];
+               a[i]=a[j];
+               a[j]=t;
+            }
+        }
+    }
+    printf("The asanding order\n\n");
+    for(j=1;j<=n;j++)
+        printf("%d ",a[j]);
     return 0;
 }
