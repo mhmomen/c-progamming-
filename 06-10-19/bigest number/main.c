f4753c7404c3e9747bc902ba5aec80f76a8255b9
momen <hemelmomin520@gmail.com>
2019-10-06 20:24:20 +0600

    biggest number

--- 06-10-19/bigest number/main.c
+++ 06-10-19/bigest number/main.c
@@ -3,6 +3,18 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    int a,b,c;
+    printf("Enter three number: ");
+    scanf("%d%d%d",&a,&b,&c);
+
+    if(a>b&&a>c)
+        printf("the biggest number is %d",a);
+   else if(b>a&&b>c)
+            printf("the biggest number is %d",b);
+    else if(c>a&&c>b)
+        printf("the biggest number is %d",c);
+    else if(a==b&&b==c)
+        printf("%d and %d and %D are equal",a,b,c);
+
     return 0;
 }
