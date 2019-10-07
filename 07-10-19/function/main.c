33814b6ccf108148c61f8f6fa559f5242f4e88ec
momen <hemelmomin520@gmail.com>
2019-10-07 17:07:38 +0600

    function1

---
+++ 07-10-19/function/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+void momen(int a, int b)
+{
+  if (a>b)
+        printf("the biggest number is %d",a);
+    else if(b>a)
+    printf("the biggest number is %d",b);
+  else
+    printf("%d and %d is equal",a,b);
+}
+
+int main()
+{
+    int n1,n2;
+    printf("Enter first number: ");
+    scanf("%d",&n1);
+    printf("Enter secend number: ");
+    scanf("%d",&n2);
+
+    momen(n1,n2);
+    return 0;
+}
