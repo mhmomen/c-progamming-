8702345f9fb0690a0d3d2e3fe3a5c8f5b5228157
momen <hemelmomin520@gmail.com>
2019-10-09 19:16:44 +0600

    string added

---
+++ 09-10-19/string/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char s[20];
+    printf("Enter the string: ");
+    gets(s);
+
+    printf("\nthe full name is : %s",s);
+    return 0;
+}
