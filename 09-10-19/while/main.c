b26cd48827160185a9bc5ba6c56d1976673c5c2e
momen <hemelmomin520@gmail.com>
2019-10-09 17:04:07 +0600

    while

---
+++ 09-10-19/while/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    printf("Enter the number: ");
+    scanf("%d",&n);
+
+    while(1<=n)
+    {
+        printf("hello momen\n");
+        n--;
+    }
+
+    return 0;
+}
