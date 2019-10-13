9f1f473bf8dc2da406448b3f25a55ed37f544f82
momen <hemelmomin520@gmail.com>
2019-10-13 21:06:20 +0600

    add,sub,multi,divi,

--- 13-10-19/ass-pb-8/main.c
+++ 13-10-19/ass-pb-8/main.c
@@ -8,15 +8,13 @@ int main()
     scanf("%c",&L);
 
     if(L>='a'&&L<='z')
-        printf("it a small leter");
+        printf("it is a small letter.");
     else if(L>='A'&&L<='Z')
-        printf("it is a capital letter");
+        printf("it is a capital letter.");
     else if(L>='0'&&L<='9')
         printf("it is a digit.");
 
     else
         printf("invalid character!!");
-
-
     return 0;
 }
---
+++ 13-10-19/ass-pb-9/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    printf("chose the option:\n\n addition (+)\n subtraction(-)\n multiplication(*)\n division(/)\n\n");
+    scanf("%c",&ch);
+
+    float num1,num2;
+    printf("Enter 1st number : ");
+    scanf("%d",&num1);
+    printf("Enter 2st number : ");
+    scanf("%d",&num2);
+
+    switch(ch)
+    {
+        case '+':
+            printf("the result:%f",num1+num2);
+            break;
+        case '-':
+            printf("the result:%f",num1-num2);
+            break;
+        case '*':
+            printf("the result:%f",num1*num2);
+            break;
+        case '/':
+            printf("the result:%.2f",num1/num2);
+            break;
+        default:
+            printf("Invalid option!!");
+    }
+    return 0;
+}
