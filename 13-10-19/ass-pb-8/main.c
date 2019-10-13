ea81bdd6d487a557a530e63e08259e39e003e936
momen <hemelmomin520@gmail.com>
2019-10-13 19:58:20 +0600

    charercter

---
+++ 13-10-19/ass-pb-8/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char L;
+    printf("Enter a a character: ");
+    scanf("%c",&L);
+
+    if(L>='a'&&L<='z')
+        printf("it a small leter");
+    else if(L>='A'&&L<='Z')
+        printf("it is a capital letter");
+    else if(L>='0'&&L<='9')
+        printf("it is a digit.");
+
+    else
+        printf("invalid character!!");
+
+
+    return 0;
+}
